fb39fa21cfd0e0a7d8b5d46bfabd025200d1000f
kendo5731 <aur.baumann@gmail.com>
2016-04-23 13:01:52 +0200

    adds test for QuestionManager.getNextQuestion

--- TuPreferesTests/QuestionManagerTest.swift
+++ TuPreferesTests/QuestionManagerTest.swift
@@ -11,16 +11,19 @@ import XCTest
 
 class QuestionManagerTest: XCTestCase {
     
+    var httpClient: MockHttpClient? = nil
     var questionManager: QuestionManager? = nil
     
     override func setUp() {
         super.setUp()
-        questionManager = QuestionManager(client: MockHttpClient())
+        httpClient = MockHttpClient()
+        questionManager = QuestionManager(client: httpClient!)
     }
     
     override func tearDown() {
         super.tearDown()
         questionManager = nil
+        httpClient = nil
     }
     
     func testCreateNewQuestion() {
@@ -38,8 +41,53 @@ class QuestionManagerTest: XCTestCase {
         XCTAssertEqual(q.choices[1].content, "Foo Bar")
         XCTAssertEqual(q.choices[1].contentShort, "Foo")
     }
+    
+    func testGetNextQuestion() {
+        /* Given */
+        let viewCtrl = MockViewController()
+        
+        /* When */
+        questionManager?.getNextQuestion(viewCtrl)
+        
+        /* Then */
+        XCTAssertEqual(httpClient?.lastCalledURL, NSURL(string: "http://api.tu-preferes.aubm.net/api/questions"))
+        XCTAssertNotNil(viewCtrl.lastQuestion)
+        XCTAssertEqual(viewCtrl.lastQuestion?.content, "Tu préfères vivre 20 ans millionaire ou toute une vie pauvre ?")
+    }
+    
+    func testGetNextQuestionWithError() {
+        /* Given */
+        httpClient?.respondWithError = true
+        let viewCtrl = MockViewController()
+        
+        /* When */
+        questionManager?.getNextQuestion(viewCtrl)
+        
+        /* Then */
+        XCTAssertEqual(httpClient?.lastCalledURL, NSURL(string: "http://api.tu-preferes.aubm.net/api/questions"))
+        XCTAssertNil(viewCtrl.lastQuestion)
+    }
+    
 }
 
 class MockHttpClient: HttpClient {
-    func get(url: NSURL, callback: (NSData?, NSError?)->() ) {}
+    var lastCalledURL: NSURL? = nil
+    var respondWithError: Bool = false
+    let data: NSData = ("[{ \"id\": \"55f891f4ae3a9170bebd9e7d\", \"content\": \"Tu préfères vivre 20 ans millionaire ou toute une vie pauvre ?\", \"published\": true, \"created_at\": \"2015-09-15T23:47:32.903+02:00\", \"modified_at\": \"2015-09-15T23:51:27.043+02:00\", \"choices\": [ { \"content\": \"Vivre 20 ans\", \"content_short\": \"20 ans\", \"slug\": \"20-ans\" }, { \"content\": \"Toute une vie\", \"content_short\": \"Une vie\", \"slug\": \"une-vie\" } ], \"statistics\": { \"total_votes\": 3, \"men_votes\": 1, \"women_votes\": 2, \"per_choice\": { \"20-ans\": { \"total_votes\": 1, \"men_votes\": 0, \"women_votes\": 1 }, \"une-vie\": { \"total_votes\": 2, \"men_votes\": 1, \"women_votes\": 1 } } } }]" as NSString).dataUsingEncoding(NSUTF8StringEncoding)!
+    
+    func get(url: NSURL, callback: (NSData?, NSError?)->() ) {
+        lastCalledURL = url
+        if respondWithError {
+            callback(nil, NSError(domain: "some error" , code: 1, userInfo: nil))
+            return
+        }
+        callback(data, nil)
+    }
+}
+
+class MockViewController: QuestionManagerDelegate {
+    var lastQuestion: Question? = nil
+    func OnNextQuestion(question: Question?) {
+        lastQuestion = question
+    }
 }
